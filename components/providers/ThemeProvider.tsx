c3a83f931c1b883fb0c9ac8b0699dfc68f53609c
0ankitkumar0 <ankitkumarboxer23@gmail.com>
2026-05-18 14:37:34 +0530

    Manually define ThemeProviderProps to fix broken package import

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -2,7 +2,18 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string | string[]
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  forcedTheme?: string
+  disableTransitionOnChange?: boolean
+  enableColorScheme?: boolean
+  nonce?: string
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
