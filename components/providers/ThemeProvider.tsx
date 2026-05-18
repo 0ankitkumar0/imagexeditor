c597f4510fec87dd28530c7f28716532987e8a33
0ankitkumar0 <ankitkumarboxer23@gmail.com>
2026-05-18 15:03:03 +0530

    fix: resolve ThemeProvider type error by importing props from next-themes

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -1,19 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: string | string[]
-  defaultTheme?: string
-  enableSystem?: boolean
-  storageKey?: string
-  forcedTheme?: string
-  disableTransitionOnChange?: boolean
-  enableColorScheme?: boolean
-  nonce?: string
-}
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
